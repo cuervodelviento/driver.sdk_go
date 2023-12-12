ed55e7904fefd1ca928f528033e45c66ccd396c6
Leonardo Galindo <galindocode@gmail.com>
2023-12-11 20:48:17 -0400

    feat: :sparkles: add user schemas

--- pkg/config/schemas.go
+++ pkg/config/schemas.go
@@ -1,7 +1,37 @@
 package config
 
-type GetChannelResponse struct {
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/getChannels.md
+type GetChannelResponse []*struct {
 	ChannelNumber int    `json:"channelNumber"`
 	ChannelName   string `json:"name"`
 	Source        string `json:"rtspSource"`
+}
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/setUsers.md
+type SetUserRequest []*struct {
+	ID       int    `json:"id"`
+	Used     bool   `json:"used"`
+	UserName string `json:"userName"`
+	Password string `json:"password"`
+}
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/setUsers.md
+type SetUserResponse error
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/deleteUser.md
+type DeleteUserRequest struct {
+	ID int `json:"id"`
+}
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/deleteUser.md
+type DeleteUserResponse error
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/getUsers.md
+type GetUserRequest struct{}
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/getUsers.md
+type GetUserResponse struct {
+	ID       int    `json:"id"`
+	Enabled  bool   `json:"enabled"`
+	UserName string `json:"userName"`
 }
