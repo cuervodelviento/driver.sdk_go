c80e651d0921bc00e98076d5460f157a6f4b4a34
Leonardo Galindo <galindocode@gmail.com>
2023-12-12 08:57:16 -0400

    feat: :sparkles: updated with new schemas

--- pkg/config/schemas.go
+++ pkg/config/schemas.go
@@ -1,19 +1,25 @@
 package config
 
 // https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/getChannels.md
-type GetChannelResponse []*struct {
+type GetChannelResponseItem struct {
 	ChannelNumber int    `json:"channelNumber"`
 	ChannelName   string `json:"name"`
 	Source        string `json:"rtspSource"`
 }
 
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/getChannels.md
+type GetChannelResponse []*GetChannelResponseItem
+
 // https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/setUsers.md
-type SetUserRequest []*struct {
+type SetUserRequestItem struct {
 	ID       int    `json:"id"`
 	Used     bool   `json:"used"`
 	UserName string `json:"userName"`
 	Password string `json:"password"`
 }
+
+// https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/setUsers.md
+type SetUserRequest []*SetUserRequestItem
 
 // https://github.com/Netsocs-Team/DevDocs/blob/main/markdown/drivers/config-schemas/setUsers.md
 type SetUserResponse error
