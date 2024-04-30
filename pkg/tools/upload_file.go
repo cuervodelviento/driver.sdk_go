1c3ee0ad208d91df10c46623c7814d580d903b22
Leonardo Galindo <galindocode@gmail.com>
2024-04-30 17:15:12 -0400

    feat: :sparkles: add tool for upload image to netsocs

---
+++ pkg/tools/upload_file.go
@@ -0,0 +1,48 @@
+package tools
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type uploadFileResponse struct {
+	Filename string `json:"filename"`
+}
+
+func UploadFileAndGetURL(driverHubHost string, driverKey string, file *os.File) (string, error) {
+	// Do something
+
+	body := &bytes.Buffer{}
+	url := fmt.Sprintf("%s/api/v1/upload", driverHubHost)
+
+	// Create a form file
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	if err != nil {
+		return "", err
+	}
+	io.Copy(part, file)
+
+	contentType := writer.FormDataContentType()
+	writer.Close()
+	res, err := http.Post(url, contentType, body)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	content, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	var response uploadFileResponse
+	if err := json.Unmarshal(content, &response); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s/public/%s", driverHubHost, response.Filename), nil
+}
